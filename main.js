3f78f54b69ac0d2b801dcbd431f20ee75da26cfe
Enrique <niofis@gmail.com>
2016-06-29 10:34:17 -0700

    initial commit

---
+++ main.js
@@ -0,0 +1,30 @@
+const electron = require('electron');
+const {app} = electron;
+const {BrowserWindow} = electron;
+
+let win;
+
+function createWindow () {
+  win = new BrowserWindow();
+  win.loadURL(`file://${__dirname}/index.html`);
+  win.webContents.openDevTools();
+
+  win.on('closed', () => {
+    win = null;
+  });
+}
+
+app.on('ready', createWindow);
+app.on('window-all-closed', () => {
+  //will not completly close if macOS
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  //on macOS app might not be completly closed and can be reopened
+  if (win === null) {
+    createWindow();
+  }
+});
