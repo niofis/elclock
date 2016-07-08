68992d2e2e7407b9a73399948154530543dd66c4
Enrique <enrique@verstand.solutions>
2016-07-08 14:09:10 -0700

    image loader

--- js/photos.js
+++ js/photos.js
@@ -11,7 +11,7 @@ var photos = (function () {
 
     stage = new PIXI.Container();
 
-    images = [{"url":"http://www.hdwallpapers.org/walls/google-abstract-HD.jpg","width":1920,"height":1080},{"url":"http://icanbecreative.com/res/AbstractBg/green_abstract-wide.jpg","width":1920,"height":1200},{"url":"http://www.hdwallpapers.in/walls/colorful_swings_abstract-HD.jpg","width":1920,"height":1080},{"url":"http://3.bp.blogspot.com/-XT-Y3vw9Udk/UVlI9jpVo5I/AAAAAAAAE5k/AEbPnXOrvS4/s1600/Abstract+Wallpaper+(43).jpg","width":1600,"height":1200},{"url":"http://www.psdgraphics.com/file/abstract-motion.jpg","width":5000,"height":3750},{"url":"http://img.wallpaperlist.com/uploads/wallpaper/files/per/perfect-purple-abstract-triangle-wallpaper-54c299fe8e70a.jpg","width":1920,"height":1200},{"url":"http://images5.fanpop.com/image/photos/31100000/Abstract-abstract-art-3D-_-31141956-1600-1200.gif","width":1600,"height":1200},{"url":"http://fc02.deviantart.net/fs71/f/2014/076/b/a/abstract_wallpaper_1080p_by_supersaejang-d7ajj1p.png","width":1920,"height":1080},{"url":"http://www.webdesignhot.com/wp-content/uploads/2014/03/Abstract-Geometric-Shapes-Colorful-Background-Vector-Illustration.jpg","width":736,"height":734},{"url":"http://www.wallpapers10.net/wp-content/uploads/images/wallpapers10.net-abstract-31.jpg","width":1920,"height":1200}];
+    //images = [{"url":"http://www.hdwallpapers.org/walls/google-abstract-HD.jpg","width":1920,"height":1080},{"url":"http://icanbecreative.com/res/AbstractBg/green_abstract-wide.jpg","width":1920,"height":1200},{"url":"http://www.hdwallpapers.in/walls/colorful_swings_abstract-HD.jpg","width":1920,"height":1080},{"url":"http://3.bp.blogspot.com/-XT-Y3vw9Udk/UVlI9jpVo5I/AAAAAAAAE5k/AEbPnXOrvS4/s1600/Abstract+Wallpaper+(43).jpg","width":1600,"height":1200},{"url":"http://www.psdgraphics.com/file/abstract-motion.jpg","width":5000,"height":3750},{"url":"http://img.wallpaperlist.com/uploads/wallpaper/files/per/perfect-purple-abstract-triangle-wallpaper-54c299fe8e70a.jpg","width":1920,"height":1200},{"url":"http://images5.fanpop.com/image/photos/31100000/Abstract-abstract-art-3D-_-31141956-1600-1200.gif","width":1600,"height":1200},{"url":"http://fc02.deviantart.net/fs71/f/2014/076/b/a/abstract_wallpaper_1080p_by_supersaejang-d7ajj1p.png","width":1920,"height":1080},{"url":"http://www.webdesignhot.com/wp-content/uploads/2014/03/Abstract-Geometric-Shapes-Colorful-Background-Vector-Illustration.jpg","width":736,"height":734},{"url":"http://www.wallpapers10.net/wp-content/uploads/images/wallpapers10.net-abstract-31.jpg","width":1920,"height":1200}];
 
     current = 0;
     time = new Date().getTime();
@@ -49,18 +49,23 @@ var photos = (function () {
 
     current = (current + 1) % images.length;
 
-    var texture = PIXI.Texture.fromImage(img.url, true, PIXI.SCALE_MODES.LINEAR);
-    var sprite = new PIXI.Sprite(texture);
+    var loader = new PIXI.loaders.Loader();
+    loader.add('image', img.url);
+    loader.once('complete', function (e) {
+      var sprite = new PIXI.Sprite(e.resources.image.texture);
 
-    ratio = Math.min(640/img.width,
-                   480/img.height);
-    sprite.scale.x = ratio;
-    sprite.scale.y = ratio;
-    sprite.x = (640 - img.width*ratio) / 2;
-    sprite.y = (480 - img.height*ratio) / 2;
+      ratio = Math.min(640/img.width,
+                       480/img.height);
+                       sprite.scale.x = ratio;
+                       sprite.scale.y = ratio;
+                       sprite.x = (640 - img.width*ratio) / 2;
+                       sprite.y = (480 - img.height*ratio) / 2;
 
-    stage.removeChildren();
-    stage.addChild(sprite);
+                       stage.removeChildren();
+                       stage.addChild(sprite);
+
+    });
+    loader.load();
   }
 
   function render (renderer) {
